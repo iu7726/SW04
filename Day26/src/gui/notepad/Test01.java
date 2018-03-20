e38d2c6860a2dda9b6d58537f06ceb17eb83af81
KimMinHyeok <37571051+iu7726@users.noreply.github.com>
2018-03-20 21:39:28 +0900

    메모장 만들기

---
+++ Day26/src/gui/notepad/Test01.java
@@ -0,0 +1,46 @@
+package gui.notepad;
+
+import java.awt.Button;
+import java.awt.Frame;
+
+class Window01 extends Frame{
+	
+	
+	public Window01() {
+		this.display();
+		this.event();
+		this.menu();
+		
+		this.setSize(500, 400);
+		this.setTitle("GUI Å×½ºÆ®");
+		this.setLocation(100, 200);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+
+	private void menu() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void event() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void display() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+}
+
+public class Test01 {
+	public static void main(String[] args) {
+		Window01 window = new Window01();
+	}
+}
+
